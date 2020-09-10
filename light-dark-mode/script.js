6dfc2f1a3e00045752d0516ec6de3f071132dfd7
vadickpb <vadick1@gmail.com>
2020-09-09 23:00:27 -0500

    local storage y optimizando el codigo

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -7,29 +7,20 @@ const image3 = document.querySelector('#image3');
 const textBox = document.querySelector('#text-box');
 
 
-
-//modo oscuro
-const darkMode = () => {
-    nav.style.background = "rgb(0 0 0 / 50%)";
-    toggleIcon.children[0].textContent = "Dark Mode";
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-    textBox.style.background = "rgb(255 255 255 / 50%)";
-    image1.src = "img/undraw_proud_coder_dark.svg";
-    image2.src = "img/undraw_feeling_proud_dark.svg";
-    image3.src = "img/undraw_conceptual_idea_dark.svg"
-
+//image light or dark color
+const imageMode = (mode) => {
+    image1.src = `img/undraw_proud_coder_${mode}.svg`;
+    image2.src = `img/undraw_feeling_proud_${mode}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
 }
 
-const lightMode = () => {
-    nav.style.background = "rgb(255 255 255 / 50%)";
-    toggleIcon.children[0].textContent = "Light Mode";
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-    textBox.style.background = "rgb(0 0 0 / 50%)";
-    image1.src = "img/undraw_proud_coder_light.svg";
-    image2.src = "img/undraw_feeling_proud_light.svg";
-    image3.src = "img/undraw_conceptual_idea_light.svg"
+//function light dark mode
+const toggleDarkLightMode = (isDark) => {
+    nav.style.background = isDark ? "rgb(0 0 0 / 50%)" : "rgb(255 255 255 / 50%)";
+    textBox.style.background = isDark ? "rgb(255 255 255 / 50%)" :  "rgb(0 0 0 / 50%)";
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : "Light Mode";
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode('dark') : imageMode('light')
 }
 
 
@@ -37,13 +28,26 @@ const lightMode = () => {
 const switchTheme = (e) => {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();   
+        localStorage.setItem('theme', 'dark');
+        toggleDarkLightMode(true);   
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        localStorage.setItem('theme', 'light');
+        toggleDarkLightMode(false);
     }
 };
 
 
 //Event Listener
 toggleSwitch.addEventListener('change', switchTheme); 
+
+//save swichTheme en Local Storage
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(true);
+    }
+}
